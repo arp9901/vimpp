ebf52423dd8574156dd6ab1676f2189af3b00c8e
Arpit <arpitsuwashchandramishra@gmail.com>
2024-12-06 22:58:23 +0530

    fixes

---
+++ index.py
@@ -0,0 +1,24 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            with open("index.html", "r") as file:
+                self.wfile.write(file.read().encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
+
+def run(server_class=HTTPServer, handler_class=handler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
